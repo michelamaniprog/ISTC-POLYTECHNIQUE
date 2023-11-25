ebe09c76903a72a460c8604d90b8df636061c91f
michelamaniprog <115624470+michelamaniprog@users.noreply.github.com>
2023-11-25 19:03:28 +0000

    Add files via upload

---
+++ site AEISTC LP2 AIN AMANI michel emmanuel/script.js
@@ -0,0 +1,8 @@
+	// CHANGER ARRIERE PLAN
+		document.getElementById("bg1").addEventListener("click", function(){
+		document.body.style.backgroundImage = "url('img/pagne jig2.png')";
+		});
+
+		document.getElementById("bg2").addEventListener("click", function(){
+		document.body.style.backgroundImage = "url('img/cours.jpg')";
+		});
